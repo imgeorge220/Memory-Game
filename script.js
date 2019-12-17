7b46c7312bd3e2a4a08efea9c6ee9f124ab807d7
George Gruner <imgeorge220@gmail.com>
2019-12-17 11:59:56 -0600

    added pics and fixed middle button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", setCards());
 
 function setSessionStorage(){
     sessionStorage.setItem('cardsChecked', '[]');
+    sessionStorage.setItem('guesses', '0');
 }
 
 function setCards(){
@@ -18,18 +19,25 @@ function setCards(){
         if (i === 13) {
             var card = document.createElement("button");
             card.setAttribute("id", "middle-card");
-            card.innerHTML = "MIDDLE BUTTON"
+            card.innerText = 0;
         } else {
             var card = document.createElement("button");
             var randNo = Math.floor(Math.random() * (25 - i));
-            card.innerText = deck[randNo];
-            card.setAttribute('hiddenno', deck[randNo]);
-            deck.splice(randNo, 1);
+            card.setAttribute('hiddenid', deck[randNo]);
             card.setAttribute("id", "card" + i);
             card.setAttribute("onclick", "revealCard(" + i + ")");
             card.setAttribute("locked", "false");
+            var icon = document.createElement("img");
+            var srcNo = Math.floor(deck[randNo]) + ".png";
+            icon.setAttribute("src", srcNo);
+            icon.setAttribute("id", 'picid' + deck[randNo])
+            icon.classList.add("card-image")
+            card.appendChild(icon);
+            deck.splice(randNo, 1);
         }
         card.classList.add("card", "card-hidden"); 
+        console.log(rowItem)
+        console.log(card)
         rowItem.appendChild(card);
     }
     
@@ -41,38 +49,37 @@ function revealCard(cardNo){
         card.setAttribute("locked", "true");
         card.classList.add("card-revealed");
         card.classList.remove("card-hidden");
+        card.innerText = card.getAttribute('hiddenid');
         var cardsChecked = JSON.parse(sessionStorage.getItem('cardsChecked'));
         cardsChecked.push(cardNo);
         sessionStorage.setItem('cardsChecked', JSON.stringify(cardsChecked));
-        if (cardsChecked.length === 2){
-            var card1 =  cardsChecked[0];
-            var card2 = cardsChecked[1];
+        if (cardsChecked.length % 2 === 0){
+            var card1 =  cardsChecked[cardsChecked.length-2];
+            var card2 = cardsChecked[cardsChecked.length-1];
+            sessionStorage.setItem('guesses', Number(sessionStorage.getItem('guesses')) + 1);
+            document.getElementById('middle-card').innerText = sessionStorage.getItem('guesses');
             window.setTimeout(checkMatch, 1000, card1, card2);
         }
     }
 }
 
 function checkMatch(card1,card2){
-    console.log('1: ' + card1 + ' 2: ' + card2)
     var card1Item = document.getElementById('card' + card1);
-    card1Number = card1Item.getAttribute('hiddenno');
+    card1Number = card1Item.getAttribute('hiddenid');
     var card2Item = document.getElementById('card' + card2);
-    card2Number = card2Item.getAttribute('hiddenno');
+    card2Number = card2Item.getAttribute('hiddenid');
     if(card1Number !== card2Number){
         card1Item.setAttribute("locked", "false");
         card2Item.setAttribute("locked", "false");
         hideCard(card1);
         hideCard(card2);
     }
-    sessionStorage.setItem('cardsChecked', '[]');
 }
 
-
-
 function hideCard(cardNo) {
-    
     var card = document.getElementById("card" + cardNo);
     card.classList.add("card-hidden");
     card.classList.remove("card-revealed");
+    card.innerText = '';
     card.setAttribute("onclick", "revealCard(" + cardNo + ")");
 }
