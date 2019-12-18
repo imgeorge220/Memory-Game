4a0aa722648e6690075315f1bf34603d26fec079
George Gruner <imgeorge220@gmail.com>
2019-12-18 13:50:27 -0600

    high score counter

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.getElementById('reset-button').addEventListener("click", function () {l
 function setSessionStorage(){
     sessionStorage.setItem('cardsChecked', '[]');
     sessionStorage.setItem('guesses', '0');
+    sessionStorage.setItem('matches', '0');
+    // localStorage.setItem('max', 100);
 }
 
 function setCards(){
@@ -19,19 +21,23 @@ function setCards(){
         if (i === 13) {
             var container = document.createElement("div");
             var counter = document.createElement("div");
+            var max = document.createElement("div");
             var reset = document.createElement("button");
             container.setAttribute("id", "middle-card");
             counter.innerText = 0;
+            counter.setAttribute("id", "counter");
             reset.innerText = "Reset";
             reset.setAttribute("id", "reset-button");
-            counter.setAttribute("id", "counter");
+            max.innerText = "High Score: " + localStorage.getItem("max");
+            max.setAttribute("id", "high-score");
             container.appendChild(reset);
             container.appendChild(counter);
+            container.appendChild(max);
         } else {
             var container = document.createElement("div");
+            container.classList.add("card-container");
             var card = document.createElement("div");
             var randNo = Math.floor(Math.random() * (25 - i));
-            container.classList.add("card-container")
             card.setAttribute('hiddenid', deck[randNo]);
             card.setAttribute("id", "card" + i);
             card.setAttribute("onclick", "revealCard(" + i + ")");
@@ -40,10 +46,11 @@ function setCards(){
             var cardFront = document.createElement("div");
             var cardBack = document.createElement("div");
             var icon = document.createElement("img");
-            var srcNo = Math.floor(deck[randNo]) + ".png";
+            var srcNo = "/" + Math.floor(+ deck[randNo]) + ".png";
             icon.setAttribute("src", srcNo);
             icon.classList.add("card-image")
             cardFront.classList.add("card-hidden", "card-flipping");
+            cardFront.innerText = deck[randNo];
             cardBack.classList.add("card-revealed", "card-flipping");
             cardBack.appendChild(icon);
             card.appendChild(cardFront);
@@ -84,7 +91,22 @@ function checkMatch(card1,card2){
         card2Item.setAttribute("locked", "false");
         hideCard(card1);
         hideCard(card2);
+    } else {
+        var matches = Number(sessionStorage.getItem("matches")) + 1;
+        sessionStorage.setItem("matches", matches);
+        var max = Number(localStorage.getItem("max"));
+        var guesses = Number(sessionStorage.getItem("guesses"));
+        if (matches === 12){
+            var highScore = '';
+            if(guesses < max){
+                localStorage.setItem("max", guesses);
+                document.getElementById("high-score").innerText = "High Score: " + guesses;
+                highScore = " You got the new High Score!"
+            }
+            window.alert("Congratulations! It only took you " + guesses + " tries!" + highScore)
+        }
     }
+
 }
 
 function hideCard(cardNo) {
